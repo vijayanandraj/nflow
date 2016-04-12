1179e2e613d708b4905e17147dd1cd3e681e6a69
Edvard Fonsell <edvard.fonsell@nitorcreations.com>
2016-04-12 16:51:42 +0300

    rename package com.nitorcreations.nflow -> io.nflow

---
+++ nflow-engine/src/main/java/io/nflow/engine/workflow/definition/WorkflowDefinitionStatistics.java
@@ -0,0 +1,6 @@
+package io.nflow.engine.workflow.definition;
+
+public class WorkflowDefinitionStatistics {
+  public long allInstances;
+  public long queuedInstances;
+}
---
+++ nflow-engine/src/test/java/io/nflow/engine/internal/dao/DaoTestConfiguration.java
@@ -0,0 +1,81 @@
+package io.nflow.engine.internal.dao;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+import io.nflow.engine.internal.config.NFlow;
+import io.nflow.engine.internal.storage.db.H2DatabaseConfiguration;
+import io.nflow.engine.internal.storage.db.H2DatabaseConfiguration.H2SQLVariants;
+
+@Configuration
+@PropertySource({"classpath:junit.properties"})
+@EnableTransactionManagement
+@Import(H2DatabaseConfiguration.class)
+public class DaoTestConfiguration {
+
+  @Bean
+  public WorkflowInstanceDao workflowInstanceDao() {
+    return new WorkflowInstanceDao();
+  }
+
+  @Bean
+  public WorkflowDefinitionDao workflowDefinitionDao() {
+    return new WorkflowDefinitionDao();
+  }
+
+  @Bean
+  public ExecutorDao executorDao(Environment env) {
+    ExecutorDao dao = new ExecutorDao();
+    dao.setSQLVariants(new H2SQLVariants());
+    dao.setEnvironment(env);
+    return dao;
+  }
+
+  @Bean
+  public StatisticsDao statisticsDao() {
+    return new StatisticsDao();
+  }
+
+  @Bean
+  public ArchiveDao archiveDao() {
+    return new ArchiveDao();
+  }
+
+  @Bean
+  public HealthCheckDao healthCheckDao() {
+    return new HealthCheckDao();
+  }
+
+  @Bean
+  public TableMetadataChecker tableMetadataChecker() {
+    return new TableMetadataChecker();
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource ds) {
+    return new DataSourceTransactionManager(ds);
+  }
+
+  @Bean
+  @NFlow
+  public ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.setSerializationInclusion(NON_EMPTY);
+    mapper.registerModule(new JodaModule());
+    return mapper;
+  }
+}
+
